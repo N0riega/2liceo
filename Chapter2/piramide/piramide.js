294b84406fdd535156c6ec23cdf791d6cb70074b
N0riega <92650128+N0riega@users.noreply.github.com>
2022-03-08 14:41:51 -0500

    Add files via upload

---
+++ Chapter2/piramide/piramide.js
@@ -0,0 +1,38 @@
+function piso(nPiso, altura){
+    let texto= "<p>"
+     if(nPiso==altura){
+       for( let i=0;i<2 * altura-1;i++){
+         texto= texto + "#"
+       }
+     }else{
+       nEspIzq= altura-nPiso;
+       nEspDer= 2* nPiso-3;
+    for(let i=0;i<nEspIzq; i++){
+        texto=texto +"&nbsp;"
+    }
+    if(nPiso==1){
+      texto= texto +"#"
+    }else{
+      texto=texto +"#"
+      for(let i=0;i< nEspDer;i++){
+        texto=texto + "&nbsp"
+      }
+      texto=texto + "#"
+    }
+   }
+   texto=texto + "</p>"
+    return texto;
+ }
+  
+ 
+ let nPisos;
+ do{
+ nPisos= Number(prompt("Ingrese el numero de pisos que desea"))
+ }while(isNaN(nPisos) || nPisos<=0 || nPisos>10 || nPisos%1 !=0) 
+ let contenido = "";
+ for(let i=1; i<= nPisos; i++){
+  contenido = contenido + piso(i, nPisos)
+ }
+ let contenedor =  document.querySelector("#contenedor")
+ contenedor.innerHTML = contenido;
+ 
