58bfc493fe8b4c4b82838186786d99c9367628bf
N0riega <92650128+N0riega@users.noreply.github.com>
2022-03-08 14:48:16 -0500

    Add files via upload

---
+++ Chapter2/funciones1casa/casa.js
@@ -0,0 +1,26 @@
+let nPisos = parseInt(prompt("Ingrese un numero de pisos para la altura de su casa"))
+function piso(){
+    console.log(" ______");
+    console.log("| _  _ |");
+    console.log("||_||_||");
+    console.log("|______|");
+}
+function pisoBase(){
+    console.log(" ________");
+    console.log("|        |");
+    console.log("| __  __ |");
+    console.log("||__||__||");
+    console.log("|   __   |");
+    console.log("|__|__|__|");
+}
+function techo(){
+    console.log("  ________");
+    console.log(" /        \\");
+}
+function casanPisos(){
+   
+}
+ 
+ 
+ 
+
